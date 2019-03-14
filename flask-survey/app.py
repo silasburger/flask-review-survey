2227baf83e5368d8267dd8377d0c0b3c5780ec54
Silas Burger <ybsilas@gmail.com>
2019-03-14 16:43:47 -0700

    added support for multiple surveys

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, session, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+from surveys import surveys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
@@ -8,21 +8,38 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 debug = DebugToolbarExtension(app)
 
 
-@app.route('/instructions')
-def survey_start_view():
-    """ view function that shows the survey title, instructions, and start button."""
-
-    session['responses'] = []
-
-    return render_template('start.html', survery_title=satisfaction_survey.title, survey_description=satisfaction_survey.instructions)
+@app.route('/')
+def choose_survey_view():
+    """view function that shows a form to select which survey to take."""
+    return render_template('choose-survey.html', surveys=surveys)
 
 
-@app.route('/question/<int:question_num>')
+@app.route('/', methods=["POST"])
+def pick_survey():
+    """we choose the survey based on the response and add it to the session."""
+    survey_key = request.form.get('survey')
+    survey = surveys[survey_key]
+    session['CURRENT_SURVEY_KEY'] = survey_key
+    return render_template('start.html', survey_title=survey.title, survey_instructions=survey.instructions)
+
+
+@app.route('/questions/<int:question_num>', methods=["POST"])
 def question_view(question_num):
-    """ view funtion that shows the first question in the survey"""
+    """view funtion that shows the first question in the survey"""
+    survey_key = session.get('CURRENT_SURVEY_KEY')
+    survey = surveys[survey_key]
+    if question_num > 0:
+        question_response = request.form['question_response']
+        responses = session['responses']
+        responses.append(question_response)
+        session['responses'] = responses
+    else:
+        session["responses"] = []
+    
+    if question_num >= len(survey.questions):
+        return render_template('thank.html', survey_title=survey.title)
 
-    response = request.args['answer']
+    choices = survey.questions[question_num].choices
+    question = survey.questions[question_num].question
 
-    responses = session['responses']
-
-    render_template('question.html')
+    return render_template('question.html', question=question, choices=choices, question_num=question_num)
